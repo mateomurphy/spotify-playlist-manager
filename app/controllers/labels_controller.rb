667af66c341c0bd2b3145f5005e8cfa17e5a511f
Mateo Murphy <33degrees@gmail.com>
2021-08-08 20:42:29 -0400

    Improve styling

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,6 +1,6 @@
 require 'paginated_search'
 
-class LabelsController < ActionController::Base
+class LabelsController < ApplicationController
   def show
     @label_name = params[:id]
     @results = PaginatedSearch.new("label:\"#{@label_name}\"").sort { |a, b| b.release_date <=> a.release_date }
